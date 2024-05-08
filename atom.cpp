7d5ba56330917bb0cf485982e2802e24bbd16216
Guru-02 <gurughaligi@gmail.com>
2024-05-08 12:27:19 +0530

    Modified atom.cpp

--- atom.cpp
+++ atom.cpp
@@ -2,14 +2,14 @@
 using namespace std;  
 int main()  
 {  
-  int n,r,sum=0,temp;    
+  int n,rem,sum=0,temp;    
   cout<<"Enter the Number=";    
   cin>>n;    
  temp=n;    
  while(n>0)    
 {    
- r=n%10;    
- sum=(sum*10)+r;    
+ rem=n%10;    
+ sum=(sum*10)+rem;    
  n=n/10;    
 }    
 if(temp==sum)    
